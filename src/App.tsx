ce71209c340c3a646652e80813705b2d2450f29b
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2025-06-28 16:09:00 -0600

    add pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => {
-  const hola = "holaa";
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>

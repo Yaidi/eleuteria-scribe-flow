2d89572d89d24d1f6a4a1972aec490f922077719
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2025-07-17 21:29:09 -0600

    move router

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "@/store/config.ts";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { electron } from "@/store/electron/actions.ts";
+
+const Router = electron ? HashRouter : BrowserRouter;
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App Router={Router} />
     </Provider>
   </React.StrictMode>,
 );
--- test/unit-test/App.test.tsx
+++ test/unit-test/App.test.tsx
@@ -1,49 +1,35 @@
 import { screen } from "@testing-library/react";
-import { vi, expect, describe, test, beforeEach } from "vitest";
+import { vi, expect, describe, test } from "vitest";
 import App from "@/App.tsx";
 import { renderWithProviders } from "../utils/renderWithProviders.tsx";
 import { templates } from "../mocks/templates.ts";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 vi.mock("@/https/fetch.ts", () => ({
   getTemplates: vi.fn(() => Promise.resolve(templates)),
 }));
 
-beforeEach(() => {
-  vi.clearAllMocks();
-  vi.resetModules();
-});
-
 describe("App", () => {
   test("Show Welcome Page", () => {
     window.history.pushState({}, "", "/");
-    renderWithProviders(<App></App>);
+    renderWithProviders(<App Router={BrowserRouter}></App>);
     expect(window.location.pathname).toBe("/");
-    expect(screen.getByRole("heading", { name: /Eleuteria/i })).toBeTruthy();
-  });
-
-  test("should test hash router redirect to Welcome page", () => {
-    vi.doMock("@/store/electron/actions", async () => {
-      const actual = await vi.importActual<typeof import("@/store/electron/actions")>(
-        "@/store/electron/actions",
-      );
-      return {
-        ...actual,
-        getElectron: () => true,
-        electron: true,
-      };
-    });
-    window.location.hash = "#/";
-    renderWithProviders(<App></App>);
-    expect(window.location.hash).toBe("#/");
     expect(screen.getByRole("heading", { name: /Eleuteria/i })).toBeTruthy();
   });
 
   test("Show Not Found Page when path is invalid", async () => {
     window.history.pushState({}, "", "/invalid-path");
-    renderWithProviders(<App />);
+    renderWithProviders(<App Router={BrowserRouter} />);
     expect(window.location.pathname).toBe("/invalid-path");
     expect(screen.findByText(/404/i)).toBeTruthy();
     expect(screen.getByText(/not found/i)).toBeTruthy();
     expect(screen.getByText(/Oops! Page not found/i)).toBeTruthy();
+  });
+
+  test("should test hash router redirect to Welcome page", () => {
+    window.location.hash = "#/";
+    renderWithProviders(<App Router={HashRouter}></App>);
+    expect(window.location.hash).toBe("#/");
+    expect(screen.getByRole("heading", { name: /Eleuteria/i })).toBeTruthy();
   });
 });
